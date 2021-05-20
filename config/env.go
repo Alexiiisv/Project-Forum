3035c7034be3c92763669467574572595c7b95c0
Alexiiisv <56995052+Alexiiisv@users.noreply.github.com>
2021-05-20 14:32:30 +0200

    very basic page

---
+++ config/env.go
@@ -0,0 +1,4 @@
+package config
+
+// Initiliazing the port
+const LocalhostPort = ":8080"
---
+++ main.go
@@ -0,0 +1,41 @@
+package main //Main code used to run the server
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	config "./config"
+)
+
+type Data struct {
+	Name string
+}
+
+var Dataa = Data{"alexis"}
+
+func main() {
+
+	fmt.Println("Please connect to\u001b[31m localhost", config.LocalhostPort, "\u001b[0m")
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets")))) // Join Assets Directory to the server
+	http.HandleFunc("/", troll)
+	http.HandleFunc("/locations", troll)
+	http.HandleFunc("/dates", troll)
+	http.HandleFunc("/bestdate", troll)
+	http.HandleFunc("/relation", troll)
+	http.HandleFunc("/artists", troll)
+	http.HandleFunc("/singleArtist", troll)
+	http.HandleFunc("/countryConcert", troll)
+	http.HandleFunc("/Loc&date", troll)
+	err := http.ListenAndServe(config.LocalhostPort, nil) // Set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func troll(w http.ResponseWriter, r *http.Request) {
+	t := template.New("index-template")
+	t = template.Must(t.ParseFiles("index.html"))
+	t.ExecuteTemplate(w, "index", Dataa)
+	fmt.Fprintln(w, Dataa.Name)
+}
