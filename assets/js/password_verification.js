e2d9a3c0193ebdfabce3a20a291a669824597347
ADUCOUDRE <antoine.ducoudre@ynov.com>
2021-05-21 19:52:47 +0200

    passwd verif for Elodie

--- assets/js/password_verification.js
+++ assets/js/password_verification.js
@@ -1,36 +1,57 @@
-function checkPass()
-{
-    console.log("Hello")
-    var pass1 = document.getElementById('pass1');
-    //var pass2 = document.getElementById('pass2');
-    var message = document.getElementById('error-nwl');
-    var goodColor = "#66cc66";
-    var badColor = "#ff6666";
- 	
-    if(pass1.value.length > 5)
-    {
-        pass1.style.backgroundColor = goodColor;
-        message.style.color = goodColor;
-        message.innerHTML = "character number ok!"
-    }
-    else
-    {
-        pass1.style.backgroundColor = badColor;
-        message.style.color = badColor;
-        message.innerHTML = " you have to enter at least 6 digit!"
-        return;
-    }
-  
-    // if(pass1.value == pass2.value)
-    // {
-    //     pass2.style.backgroundColor = goodColor;
-    //     message.style.color = goodColor;
-    //     message.innerHTML = "ok!"
-    // }
-	// else
-    // {
-    //     pass2.style.backgroundColor = badColor;
-    //     message.style.color = badColor;
-    //     message.innerHTML = " These passwords don't match"
-    // }
-} 
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
